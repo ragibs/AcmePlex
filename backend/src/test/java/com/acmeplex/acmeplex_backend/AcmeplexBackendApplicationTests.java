da52f7232b341a52e6637c0dbf5a0c05566feb51
ragib sina <ragib.sina@gmail.com>
2024-11-14 16:04:55 -0700

    added backend boilerplate

---
+++ backend/src/test/java/com/acmeplex/acmeplex_backend/AcmeplexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmeplex.acmeplex_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeplexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
