0079ee778a1ce0d9bd7fa8f5248a1ab4497c30f6
ragib sina <ragib.sina@gmail.com>
2024-11-17 15:27:22 -0700

    #3 added exception for theatre, added global exception, added service folder, added get theatrebyid

--- backend/src/main/java/com/acmeplex/acmeplex_backend/controller/TheatreController.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/controller/TheatreController.java
@@ -1,10 +1,12 @@
 package com.acmeplex.acmeplex_backend.controller;
 
 import com.acmeplex.acmeplex_backend.model.Theatre;
-import com.acmeplex.acmeplex_backend.repository.TheatreRepository;
+import com.acmeplex.acmeplex_backend.service.TheatreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,11 +16,16 @@ import java.util.List;
 public class TheatreController {
 
     @Autowired
-    private TheatreRepository theatreRepository;
+    private TheatreService theatreService;
 
     @GetMapping("/theatres")
     public List<Theatre> getAllTheatre(){
-        return theatreRepository.findAll();
+        return theatreService.getAllTheatre();
     }
 
+    @GetMapping("/theatre/{id}")
+    public ResponseEntity<Theatre> getTheatreById(@PathVariable Long id){
+        Theatre theatre = theatreService.getTheatreById(id);
+        return ResponseEntity.ok(theatre);
+    }
 }
--- backend/src/main/java/com/acmeplex/acmeplex_backend/model/Theatre.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/model/Theatre.java
@@ -2,12 +2,13 @@ package com.acmeplex.acmeplex_backend.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Theatre {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String address;
