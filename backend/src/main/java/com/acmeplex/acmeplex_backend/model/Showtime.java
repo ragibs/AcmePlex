39f61215e468c3f25548aee3a91effd5afe47d3c
Heemin Kang <heeminkang1@gmail.com>
2024-11-18 19:35:23 -0700

    #7 added showtime model, service, and controller

---
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/model/Showtime.java
@@ -0,0 +1,18 @@
+package com.acmeplex.acmeplex_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Showtime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime startTime;
+}
