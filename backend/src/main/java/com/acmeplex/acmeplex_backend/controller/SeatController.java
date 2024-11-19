372567f45551b300611a1a471d50e837eb7e6c55
Heemin Kang <heeminkang1@gmail.com>
2024-11-18 21:57:48 -0700

    Add crossorigin for seat and showtime controllers

--- backend/src/main/java/com/acmeplex/acmeplex_backend/controller/SeatController.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/controller/SeatController.java
@@ -2,11 +2,13 @@ package com.acmeplex.acmeplex_backend.controller;
 
 import com.acmeplex.acmeplex_backend.service.SeatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/seats")
+@CrossOrigin("http://localhost:3000")
 public class SeatController {
     private final SeatService seatService;
 
