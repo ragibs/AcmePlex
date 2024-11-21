d600ab6043ae5ce43ed3aad327759e3050e079e8
Pahul Brar <pahulbrar17@gmail.com>
2024-11-21 10:53:05 -0700

    Added Registered User

---
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/repository/RegisteredUserRepository.java
@@ -0,0 +1,11 @@
+package com.acmeplex.acmeplex_backend.repository;
+
+import com.acmeplex.acmeplex_backend.model.RegisteredUser;
+import com.acmeplex.acmeplex_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RegisteredUserRepository extends JpaRepository<User, Long>{
+    Optional<RegisteredUser> findByEmail(String email);
+}
