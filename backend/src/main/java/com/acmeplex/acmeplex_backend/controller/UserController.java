0959ea51403e827a23c70b3ee2b3cd0da4222e69
Pahul Brar <pahulbrar17@gmail.com>
2024-11-20 19:58:13 -0700

    worked on registration and login

--- backend/src/main/java/com/acmeplex/acmeplex_backend/controller/UserController.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/controller/UserController.java
@@ -1,14 +1,29 @@
 package com.acmeplex.acmeplex_backend.controller;
 
+import com.acmeplex.acmeplex_backend.model.User;
 import com.acmeplex.acmeplex_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user){
+        userService.register(user.getEmail(), user.getPassword());
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user){
+        String token = userService.login(user.getEmail(), user.getPassword());
+        return  new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+
 }
--- backend/src/main/java/com/acmeplex/acmeplex_backend/service/UserService.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/service/UserService.java
@@ -1,8 +1,11 @@
 package com.acmeplex.acmeplex_backend.service;
 
+import com.acmeplex.acmeplex_backend.model.User;
 import com.acmeplex.acmeplex_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -10,4 +13,23 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    public User register(String email, String password){
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+
+        return userRepository.save(user);
+    }
+
+    public String login(String email, String password) {
+        Optional<User> userOpt = userRepository.findByEmail(email);
+        if (userOpt.isPresent()){
+            User user = userOpt.get();
+            if (password.matches(user.getPassword()));
+            {
+                return "Successfully logged in"; //would be where we return the jwt token?
+            }
+        }
+        throw new RuntimeException("Invalid Email or Password");
+    }
 }
