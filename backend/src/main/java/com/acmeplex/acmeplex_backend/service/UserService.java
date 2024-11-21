334691cbdbf05c5e8d453bf60b0671ae56adee4c
Pahul Brar <pahulbrar17@gmail.com>
2024-11-21 10:53:05 -0700

    Added Registered User

--- backend/src/main/java/com/acmeplex/acmeplex_backend/service/UserService.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.acmeplex.acmeplex_backend.service;
 
+import com.acmeplex.acmeplex_backend.model.RegisteredUser;
 import com.acmeplex.acmeplex_backend.model.User;
 import com.acmeplex.acmeplex_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +14,5 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User register(String email, String password){
-        User user = new User();
-        user.setEmail(email);
-        user.setPassword(password);
 
-        return userRepository.save(user);
-    }
-
-    public String login(String email, String password) {
-        Optional<User> userOpt = userRepository.findByEmail(email);
-        if (userOpt.isPresent()){
-            User user = userOpt.get();
-            if (password.matches(user.getPassword()));
-            {
-                return "Successfully logged in"; //would be where we return the jwt token?
-            }
-        }
-        throw new RuntimeException("Invalid Email or Password");
-    }
 }
