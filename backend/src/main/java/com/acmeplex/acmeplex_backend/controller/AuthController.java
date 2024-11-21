95001a2b76457349934305d64fa796559f5a9d9d
Suchet Mangat <136658794+SuchetMangat@users.noreply.github.com>
2024-11-21 00:23:57 -0700

    Make jwt token using request body

--- backend/src/main/java/com/acmeplex/acmeplex_backend/controller/AuthController.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/controller/AuthController.java
@@ -1,26 +1,30 @@
 package com.acmeplex.acmeplex_backend.controller;
 
+import com.acmeplex.acmeplex_backend.model.LoginRequest;
 import com.acmeplex.acmeplex_backend.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthController(TokenService tokenService) {
+    public AuthController(TokenService tokenService, AuthenticationManager authenticationManager) {
         this.tokenService = tokenService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/login")
-    public String token(Authentication authentication){
-        logger.debug("Token request for user: {}", authentication.getName());
-        String token = tokenService.generateToken(authentication);
-        logger.debug("Token Granted {}", token);
-        return token;
+    public String token(@RequestBody LoginRequest loginRequest){
+        Authentication authentication= authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
+        return tokenService.generateToken(authentication);
     }
 }
