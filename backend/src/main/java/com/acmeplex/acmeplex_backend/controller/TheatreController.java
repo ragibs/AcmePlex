e89c2a1d2fbb95cac484e7e10aee6d96c3862e74
Heemin Kang <heeminkang1@gmail.com>
2024-11-23 13:49:36 -0700

    implement get showtime and movie by theatreid

--- backend/src/main/java/com/acmeplex/acmeplex_backend/controller/TheatreController.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/controller/TheatreController.java
@@ -1,6 +1,7 @@
 package com.acmeplex.acmeplex_backend.controller;
 
 import com.acmeplex.acmeplex_backend.model.Theatre;
+import com.acmeplex.acmeplex_backend.service.ShowtimeService;
 import com.acmeplex.acmeplex_backend.service.TheatreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -17,6 +19,9 @@ public class TheatreController {
 
     @Autowired
     private TheatreService theatreService;
+
+    @Autowired
+    private ShowtimeService showtimeService;
 
     @GetMapping("/theatres")
     public List<Theatre> getAllTheatre(){
@@ -27,5 +32,10 @@ public class TheatreController {
     public ResponseEntity<Theatre> getTheatreById(@PathVariable Long id){
         Theatre theatre = theatreService.getTheatreById(id);
         return ResponseEntity.ok(theatre);
+    }
+
+    @GetMapping("theatre/{theatreId}/showtimes")
+    public List<Map<String, Object>> getShowtimesForTheatre(@PathVariable Long theatreId) {
+        return showtimeService.getShowtimesByTheatre(theatreId);
     }
 }
