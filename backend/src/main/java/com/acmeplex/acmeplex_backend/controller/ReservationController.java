c1b197b49c0e23cff2b3ed7a20ac03797ec059b8
Pahul Brar <pahulbrar17@gmail.com>
2024-11-23 17:58:59 -0700

    Worked on Reservation Controller, need to first Get Ticket and Seat done, then finish it.

--- backend/src/main/java/com/acmeplex/acmeplex_backend/controller/ReservationController.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/controller/ReservationController.java
@@ -1,13 +1,57 @@
 package com.acmeplex.acmeplex_backend.controller;
 
+import com.acmeplex.acmeplex_backend.model.Reservation;
 import com.acmeplex.acmeplex_backend.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/reservations")
 @CrossOrigin("http://localhost:3000")
 public class ReservationController {
     @Autowired
     public ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService){
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation){
+        Reservation newRes = reservationService.createReservation(reservation);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newRes);
+    }
+    @GetMapping("/{id}")
+    public Optional<Reservation> getReservationByID(@PathVariable Long id) {
+        return reservationService.getReservationById(id);
+    }
+
+    public ResponseEntity<Reservation> updateReservation(
+            @PathVariable Long id,
+            @RequestBody Reservation updatedReservation) {
+        try {
+            Reservation reservation = reservationService.updateReservation(id, updatedReservation);
+            return ResponseEntity.ok(reservation);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        reservationService.deleteReservation(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    //depeneds if we need this, do we need to get all Reservations, or only for a certain movie, get them maybe
+//    public List<Reservation> getAllReservations(){
+//
+//    }
+
+
 }
--- backend/src/main/java/com/acmeplex/acmeplex_backend/service/ReservationService.java
+++ backend/src/main/java/com/acmeplex/acmeplex_backend/service/ReservationService.java
@@ -1,12 +1,48 @@
 package com.acmeplex.acmeplex_backend.service;
 
+import com.acmeplex.acmeplex_backend.model.Reservation;
 import com.acmeplex.acmeplex_backend.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationService{
 
     @Autowired
     private ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository){
+        this.reservationRepository = reservationRepository;
+    }
+
+    public Reservation createReservation(Reservation res){
+        return reservationRepository.save(res);
+    }
+
+    public Optional<Reservation> getReservationById(long Id){
+        return reservationRepository.findById(Id);
+    }
+
+    public List<Reservation> getAllReservations(){
+        return reservationRepository.findAll();
+    }
+
+    //need to implement ticket first so I can get the movie, then update it
+    public Reservation updateReservation(Long id, Reservation updatedR){
+        return reservationRepository.findById(id).map(
+                reservation -> {
+//                    reservation.setMovieName(updatedR.getMovieName());
+//                    reservation.setSeatNumber(updatedR.getSeatNumber());
+//                    reservation.setShowTime(updatedR.setShowTime());
+                    //these are just temporary stubs, until ticket is properly implemented.
+                    return reservationRepository.save(reservation);
+                }).orElseThrow(()-> new RuntimeException("Reservation Not Found"));
+    }
+
+    public void deleteReservation(Long id){
+        reservationRepository.deleteById(id);
+    }
 }
